3d95b1a50ec2d4cb23d06e97fea7ce3f5d7627ab
yunushinfowind <yunush.infowind@gmail.com>
2021-01-20 12:38:22 +0530

    change and fix issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: true,
-  // baseUrl : "http://upperneighbormusic.com:8080/api"
+  baseUrl : "http://upperneighbormusic.com:8080/api"
   // baseUrl : "http://framework.infowindtech.biz:8000/api"
-  baseUrl : "http://localhost:8080/api"
+  // baseUrl : "http://localhost:8080/api"
 };
 
 /*
